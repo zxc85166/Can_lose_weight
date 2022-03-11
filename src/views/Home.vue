5ffac1229c5ac70bc4c1d4b83b3b82280f843e20
iiiteam11801 <iiiteam11801@gmail.com>
2022-03-11 18:56:02 +0800

    環境變數

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,40 +13,49 @@ import { onMounted, ref } from "vue";
 
 // 取出後物品
 const state = ref(null);
+//使用者填入的新增資料
+const newName = ref(null);
+const newAge = ref(null);
+const newHeight = ref(null);
+const newWeight = ref(null);
+
 //憑證
 const firebaseConfig = {
-  apiKey: "AIzaSyDkm-edZzl3gWJPiCOLFCrdgZY_61oFCmI",
-  authDomain: "can-lose-weight-11cc7.firebaseapp.com",
-  projectId: "can-lose-weight-11cc7",
-  storageBucket: "can-lose-weight-11cc7.appspot.com",
-  messagingSenderId: "106861088590",
-  appId: "1:106861088590:web:4e6f32fdbe7fbf702b181f",
-  measurementId: "G-JH8RZ4FV9Z",
+  apiKey: import.meta.env.VITE_APP_apiKey,
+  authDomain: import.meta.env.VITE_APP_authDomain,
+  projectId: import.meta.env.VITE_APP_projectId,
+  storageBucket: import.meta.env.VITE_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
+  appId: import.meta.env.VITE_APP_appId,
+  measurementId: import.meta.env.VITE_APP_measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 const usersCollection = collection(db, "users");
 onMounted(() => {
   getData();
 });
 //抓取資料
 const getData = async () => {
-  const data = await getDocs(usersCollection);
-  const me = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-  state.value = me;
+  const datas = await getDocs(usersCollection);
+  const data = datas.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  state.value = data;
 };
 //輸入
 const createUser = async () => {
-  await addDoc(usersCollection, { name: "sss", age: Number(10) });
+  await addDoc(usersCollection, { name: newName.value, age: Number(newAge.value), height: Number(newHeight.value), weight: Number(newWeight.value) });
+  newName.value = null;
+  newAge.value = null;
+  newHeight.value = null;
+  newWeight.value = null;
   getData();
 };
-//更新
-const updateUser = async (id, age) => {
+//修改
+const updateUser = async (id, age, height, weight) => {
   const userDoc = doc(db, "users", id);
-  const newFields = { age: age + 1 };
+  const newFields = { age: age, height: height, weight: weight };
   await updateDoc(userDoc, newFields);
   getData();
 };
@@ -59,46 +68,95 @@ const deleteUser = async (id) => {
 </script>
 
 <template>
-  <div class="bg-gray-50">
+  <div class="bg-blue-50">
     <div
       class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8"
     >
-      <h2
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
-      >
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">棒式 / 平板支撐</span>
-          </label>
-          <label class="input-group input-group-lg">
-            <!-- <span>Price</span> -->
-            <input
-              type="text"
-              placeholder="100"
-              class="input input-bordered input-group-xs"
-            />
-            <span>秒</span>
-          </label>
-        </div>
-      </h2>
-      <div>{{ me }}</div>
+      <div class="flex gap-3">
+        <input
+          type="text"
+          placeholder="姓名"
+          v-model="newName"
+          class="input input-bordered w-20 max-w-xs text-center"
+        />
+        <input
+          type="text"
+          placeholder="年齡"
+          v-model="newAge"
+          class="input input-bordered w-20 max-w-xs text-center"
+        />
+        <input
+          type="text"
+          placeholder="身高"
+          v-model="newHeight"
+          class="input input-bordered w-20 max-w-xs text-center"
+        />
+        <input
+          type="text"
+          placeholder="體重"
+          v-model="newWeight"
+          class="input input-bordered w-20 max-w-xs text-center"
+        />
+      </div>
       <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
         <div class="inline-flex rounded-md shadow">
-          <router-link
+          <!-- <router-link
             to="/about"
             class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
-            >Next Page</router-link
-          >
+          >Next Page</router-link>-->
         </div>
-        <button @click="createUser" class="btn">按我新增</button>
+        <button @click="createUser" class="btn">新增</button>
       </div>
     </div>
-    <div v-for="i in state">
-      <h1>姓名: {{ i.name }}</h1>
-      <h1>年齡: {{ i.age }}</h1>
-      <h1>ID: {{ i.id }}</h1>
-      <button @click="updateUser(i.id, i.age)" class="btn">增加 Age</button>
-      <button @click="deleteUser(i.id)" class="btn">刪除</button>
+  </div>
+  <div class="mx-5">
+    <div class="overflow-x-auto">
+      <table class="table w-full">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>姓名</th>
+            <th>年齡</th>
+            <th>身高</th>
+            <th>體重</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- row 1 -->
+          <tr v-for="i in state" class="hover">
+            <td>{{ i.name }}</td>
+            <td>
+              <input
+                type="text"
+                v-model="i.age"
+                class="input input-bordered input-info input-sm w-11 max-w-xs"
+              />
+            </td>
+            <td>
+              <input
+                type="text"
+                v-model="i.height"
+                class="input input-bordered input-info input-sm w-14 max-w-xs"
+              />
+            </td>
+            <td>
+              <input
+                type="text"
+                v-model="i.weight"
+                class="input input-bordered input-info input-sm w-14 max-w-xs"
+              />
+            </td>
+            <td>
+              <button
+                @click="updateUser(i.id, i.age, i.height, i.weight)"
+                class="btn btn-outline btn-success mr-3"
+              >修改</button>
+              <button @click="deleteUser(i.id)" class="btn btn-outline btn-warning">刪除</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
