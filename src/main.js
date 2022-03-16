7f005703a8193dd560f55f20eec43aba906ee2e2
iiiteam11801 <iiiteam11801@gmail.com>
2022-03-16 17:48:31 +0800

    重新打包

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "./tailwind.css";
-
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import App from "./App.vue";
 import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
@@ -16,5 +17,7 @@ const router = createRouter({
   routes,
 });
 
-app.use(router).use(pinia);
+app.use(router);
+app.use(ElementPlus);
+app.use(pinia);
 app.mount("#app");
